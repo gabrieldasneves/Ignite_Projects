9f82f84714598f25be0323515dd674c2c9c82791
Gabriel das Neves <galvesdasneves@gmail.com>
2022-12-23 22:29:53 -0300

    comment component

---
+++ IgniteFeed/src/components/Comment.jsx
@@ -0,0 +1,30 @@
+import { ThumbsUp, Trash } from "phosphor-react"
+import styles from "./Comment.module.css"
+
+export function Comment(){
+    return (
+        <div className={styles.Comment}>
+            <img src="https://github.com/gabrieldasneves.png" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong></strong>
+                            <time title="21 de Dezembro às 18:32 " dateTime="2022-12-21">Publicado há </time>
+                        </div>
+                        <button title="Deletar comentário"></button>
+                        <Trash size={20} />
+                    </header>
+                    <p>Muito bom, parabéns!</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
